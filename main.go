07f3ac3e2922a309b1e4e34597d5304ca3fcc74c
Sergey Polyakov <serejka.poliakov@yandex.ru>
2020-09-29 21:48:41 +0300

    constants

--- main.go
+++ main.go
@@ -6,24 +6,24 @@ import (
 	"fmt"
 )
 
+const appName = "Golang Less"
+const (
+	_ = iota
+	B = iota
+	C = iota
+	D = iota + 5
+	E = iota
+)
+
 func main() {
-	var a int = 5
-	var b, c string = "b", "c"
-	var d bool = true
-	e := 100
-	f := "f"
-	j := true
-
-	fmt.Printf("%T - %v\n", a, a)
-	fmt.Printf("%T - %v\n", b, b)
-	fmt.Printf("%T - %v\n", c, c)
-	fmt.Printf("%T - %v\n", d, d)
-
-	fmt.Printf("%T - %v\n", e, e)
-	fmt.Printf("%T - %v\n", f, f)
-	fmt.Printf("%T - %v\n", j, j)
-
+	fmt.Println(B)
+	fmt.Println(C)
+	fmt.Println(D)
+	fmt.Println(E)
+	x, _, _ := TepesGo()
+	fmt.Println(x)
 }
-func inc() {
-	fmt.Println(e)
+
+func TepesGo() (int, bool, string) {
+	return 100, false, "string"
 }
