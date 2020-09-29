7f5eff820399ca6b8f623693860473736e8414c7
Sergey Polyakov <serejka.poliakov@yandex.ru>
2020-09-29 22:52:19 +0300

    for\switch

--- main.go
+++ main.go
@@ -2,28 +2,39 @@
 // Click here and start typing.
 package main
 
-import (
-	"fmt"
-)
-
-const appName = "Golang Less"
-const (
-	_ = iota
-	B = iota
-	C = iota
-	D = iota + 5
-	E = iota
-)
+import "fmt"
 
 func main() {
-	fmt.Println(B)
-	fmt.Println(C)
-	fmt.Println(D)
-	fmt.Println(E)
-	x, _, _ := TepesGo()
-	fmt.Println(x)
-}
 
-func TepesGo() (int, bool, string) {
-	return 100, false, "string"
+	a, b, c := 10, 20, 30
+	if a > b {
+		fmt.Println("a")
+	} else if b > c {
+		fmt.Println("b")
+	} else {
+		fmt.Println("c")
+	}
+	text := "admin"
+	switch text {
+	case "admin":
+		fmt.Println("Cовпадает")
+		fallthrough
+	case "odmin":
+		fmt.Println("Не совпадает2")
+	case "odmi2n":
+		fmt.Println("Не совпадает3")
+	default:
+		fmt.Println("SOME LOGIN")
+	}
+
+	for x := 1; x < 100; x++ {
+		if x%10 == 0 {
+			fmt.Println("Число", x)
+		}
+		fmt.Println(x)
+	}
+	x := 1
+	for x > 0 {
+		fmt.Println("x>0")
+	}
 }
