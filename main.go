06ab50c357a6c008f163b613ee19c0d5d0f5e531
Sergey Polyakov <serejka.poliakov@yandex.ru>
2020-09-30 13:33:40 +0300

    maps

--- main.go
+++ main.go
@@ -1,40 +1,23 @@
-// You can edit this code!
-// Click here and start typing.
 package main
 
 import "fmt"
 
 func main() {
+	keys := []string{}
 
-	a, b, c := 10, 20, 30
-	if a > b {
-		fmt.Println("a")
-	} else if b > c {
-		fmt.Println("b")
-	} else {
-		fmt.Println("c")
-	}
-	text := "admin"
-	switch text {
-	case "admin":
-		fmt.Println("Cовпадает")
-		fallthrough
-	case "odmin":
-		fmt.Println("Не совпадает2")
-	case "odmi2n":
-		fmt.Println("Не совпадает3")
-	default:
-		fmt.Println("SOME LOGIN")
+	carta := map[string][]int{
+		"1": {1, 2},
+		"2": {3, 4},
 	}
 
-	for x := 1; x < 100; x++ {
-		if x%10 == 0 {
-			fmt.Println("Число", x)
-		}
-		fmt.Println(x)
+	//Перебор мапа
+	for _, val := range carta {
+		// keys = append(keys, key)
+		fmt.Println(val)
 	}
-	x := 1
-	for x > 0 {
-		fmt.Println("x>0")
-	}
+	fmt.Println(keys)
+
+	//Создание мапа через мейк
+	h := make(map[int]string)
+	fmt.Println(h)
 }
